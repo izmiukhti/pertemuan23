3c5b751e2f412f1118fa112841fd57acd9e1716b
izmi <izmiukhti@gmail.com>
2024-08-18 13:13:56 +0700

    first commit

---
+++ resources/views/layouts/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.master')
+
+@section('title', 'Halaman Home')
+
+@section('isi')
+    <h1>Selamat datang HMTI PSDKU Lumajang</h1>
+@endsection
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>@yield('title')</title>
+    </head>
+    <body>
+        @include('partials.app')
+        <div class="container">
+        </div>
+    </body>
+</html>
